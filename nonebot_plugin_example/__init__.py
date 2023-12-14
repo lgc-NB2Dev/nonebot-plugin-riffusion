487fc82b1464a9a52fa76d5a8e3265405e50acde
student_2333 <lgc2333@126.com>
2023-12-15 05:37:51 +0800

    Initial commit

---
+++ nonebot_plugin_example/__init__.py
@@ -0,0 +1,18 @@
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters, require
+
+require("nonebot_plugin_alconna")
+
+from . import __main__ as __main__  # noqa: E402
+from .config import ConfigModel  # noqa: E402
+
+__version__ = "0.1.0"
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-example",
+    description="插件模板",
+    usage="这是一个一个一个插件模板",
+    type="application",
+    homepage="https://github.com/owner/nonebot-plugin-example",
+    config=ConfigModel,
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    extra={"License": "MIT", "Author": "student_2333"},
+)
